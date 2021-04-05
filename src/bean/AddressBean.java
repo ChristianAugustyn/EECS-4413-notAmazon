11edb23fc25c5ba222cdf652d9601d118bf5ad1c
jbg93 <80637472+jbg93@users.noreply.github.com>
2021-04-05 16:13:17 -0400

    Update AddressBean.java

--- src/bean/AddressBean.java
+++ src/bean/AddressBean.java
@@ -2,28 +2,49 @@ package bean;
 
 public class AddressBean {
 	private int id;
+	private String lname;
+	private String fname;
 	private String street;
+	private String city;
 	private String province;
 	private String country;
 	private String zip;
 	private String phone;
-	
-	public AddressBean(int id, String street, String province, String country, String zip, String phone) {
+	private String addressType;
+
+	public AddressBean(int id, String lname, String fname, String street, String city, String province, String country,
+			String zip, String phone, String addressType) {
 		super();
 		this.id = id;
+		this.lname = lname;
+		this.fname = fname;
 		this.street = street;
+		this.city = city;
 		this.province = province;
 		this.country = country;
 		this.zip = zip;
 		this.phone = phone;
+		this.addressType = addressType;
 	}
 
 	public int getId() {
 		return id;
 	}
 
+	public String getLname() {
+		return lname;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
 	public String getStreet() {
 		return street;
+	}
+
+	public String getCity() {
+		return city;
 	}
 
 	public String getProvince() {
@@ -40,6 +61,10 @@ public class AddressBean {
 
 	public String getPhone() {
 		return phone;
+	}
+
+	public String getAddressType() {
+		return addressType;
 	}
 	
 	
