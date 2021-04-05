37cb53797013ef6d897c10a2ba65412130f5dabe
jbg93 <80637472+jbg93@users.noreply.github.com>
2021-04-05 19:50:07 -0400

    Added UsersBean

---
+++ src/bean/UsersBean.java
@@ -0,0 +1,53 @@
+package bean;
+
+public class UsersBean {
+	private String userId;
+	private String userPw;
+	private String lname;
+	private String fname;
+	private int shipping;
+	private int billing;
+	private String token;
+	
+	public UsersBean(String userId, String userPw, String lname, String fname, int shipping, int billing,
+			String token) {
+		super();
+		this.userId = userId;
+		this.userPw = userPw;
+		this.lname = lname;
+		this.fname = fname;
+		this.shipping = shipping;
+		this.billing = billing;
+		this.token = token;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public String getUserPw() {
+		return userPw;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public int getShipping() {
+		return shipping;
+	}
+
+	public int getBilling() {
+		return billing;
+	}
+
+	public String getToken() {
+		return token;
+	}
+	
+	
+}
